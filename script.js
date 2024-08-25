26334262d397992cfde171b7f604390abcc447a8
Nazeli-git <nazeli.hovhannisyan23@gmail.com>
2024-08-25 14:15:42 +0400

    On project we added functions

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 "use strict";
  
- const countOfFilms= prompt("Քանի՞ ֆիլմ եք դիտել");
+let countOfFilms;
 
- const user = {
+function init() {
+  countOfFilms= +prompt("Քանի՞ ֆիլմ եք դիտել");
+  while (countOfFilms==isNaN || countOfFilms== "" || countOfFilms== null) {
+    countOfFilms= +prompt("Քանի՞ ֆիլմ եք դիտել");
+  }
+}
+
+init();
+
+const user = {
   count: countOfFilms,
   actors: {},
   gender: [],
@@ -11,28 +20,39 @@
   status: null
 };
 
-for (let i = 0; i < 2; i++) {
-        
-  const filmName = prompt("Որ ֆիլմն եք վերջերս դիտել");
-  const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
+function rememberUserFilms() {
+  for (let i = 0; i < 2; i++) {
+    const filmName = prompt("Որ ֆիլմն եք վերջերս դիտել");
+    const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
   
-  if (filmName != null && filmName != '' && filmRate != '' && filmName.length < 50) {
-    user.movies[filmName]= filmRate;
-    console.log('Resolve');
-  }else{
+    if (filmName != null && filmName != '' && filmRate != '' && filmName.length < 50) {
+      user.movies[filmName]= filmRate;
+      console.log('Resolve');
+    }else{
      console.log('Reject');
      i--
+    }
   }
 }
-if(user.count < 10){
-  user.status='Դուք քիչ ֆիլմեր եք դիտում'
-}else if (user.count >= 10 && user.status < 30) {
-  user.status='Դուք դասական ֆիլմ դիտող եք'
-} else if(user.count > 30){
-  user.status='Դուք կինոման եք'
-}else{
-  console.log("Տեղի է ունեցել խնդիր")
+
+function userPersonalLevel() {
+  if(user.count < 10){
+    user.status='Դուք քիչ ֆիլմեր եք դիտում'
+  }else if (user.count >= 10 && user.status < 30) {
+    user.status='Դուք դասական ֆիլմ դիտող եք'
+  } else if(user.count > 30){
+    user.status='Դուք կինոման եք'
+  }else{
+    console.log("Տեղի է ունեցել խնդիր")
+  }
+}    
+
+function userPrivat(state) {
+  state ? console.log("Sorry but user privat is not visible"): console.log(user);;
 }
 
-console.log(user);
+rememberUserFilms();
+userPersonalLevel();
+userPrivat(user.privat)
+
 
