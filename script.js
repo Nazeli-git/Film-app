2f86e839bc1439c8bef6dfbfd78a3ba0538e3a06
Nazeli-git <nazeli.hovhannisyan23@gmail.com>
2024-08-27 12:19:02 +0400

    Changed file and all application we wraped in object, and used THIS

--- script.js
+++ script.js
@@ -1,58 +1,61 @@
 "use strict";
  
-let countOfFilms;
-
-function init() {
-  countOfFilms= +prompt("Քանի՞ ֆիլմ եք դիտել");
-  while (countOfFilms==isNaN || countOfFilms== "" || countOfFilms== null) {
-    countOfFilms= +prompt("Քանի՞ ֆիլմ եք դիտել");
-  }
-}
-
-init();
 
 const user = {
-  count: countOfFilms,
+  count: 0,
   actors: {},
-  gender: [],
+  genres: [],
   privat: false,
   movies: {},
-  status: null
-};
-
-function rememberUserFilms() {
-  for (let i = 0; i < 2; i++) {
+  status: null,
+  init() {
+  this.count= +prompt("Քանի՞ ֆիլմ եք դիտել");
+  while (this.count==isNaN(this.count) || this.count== "" || this.count== null) {
+    this.count= +prompt("Քանի՞ ֆիլմ եք դիտել");
+  }
+},
+  rememberUserFilms() {
+  for (let i = 1; i < 2; i++) {
     const filmName = prompt("Որ ֆիլմն եք վերջերս դիտել");
     const filmRate = +prompt(`Ինչքա՞ն եք գնահատում ${filmName} ֆիլմը`);
   
     if (filmName != null && filmName != '' && filmRate != '' && filmName.length < 50) {
-      user.movies[filmName]= filmRate;
+      this.movies[filmName]= filmRate;
       console.log('Resolve');
     }else{
      console.log('Reject');
      i--
     }
   }
-}
-
-function userPersonalLevel() {
-  if(user.count < 10){
-    user.status='Դուք քիչ ֆիլմեր եք դիտում'
-  }else if (user.count >= 10 && user.status < 30) {
-    user.status='Դուք դասական ֆիլմ դիտող եք'
-  } else if(user.count > 30){
-    user.status='Դուք կինոման եք'
+},
+  userPersonalLevel() {
+  if(this.count < 10){
+    this.status='Դուք քիչ ֆիլմեր եք դիտում'
+  }else if (this.count >= 10 && this.status < 30) {
+    this.status='Դուք դասական ֆիլմ դիտող եք'
+  } else if(this.count > 30){
+    this.status='Դուք կինոման եք'
   }else{
     console.log("Տեղի է ունեցել խնդիր")
   }
-}    
+},
+   userPrivat(state) {
+  state ? console.log("Sorry but this privat is not visible"): console.log(this);;
+},
+  useGenres() {
+  for(let i = 0; i< 2; i++ ){
+    this.genres.push(prompt(`Ձեր նախընտրելի ժանրը ${i+1} `))
+  }
+},
+  startUp(){
+   this.init();
+   this.useGenres();
+   this.rememberUserFilms();
+   this.userPersonalLevel();
+   this.userPrivat(this.privat)
+  }
+};  
 
-function userPrivat(state) {
-  state ? console.log("Sorry but user privat is not visible"): console.log(user);;
-}
-
-rememberUserFilms();
-userPersonalLevel();
-userPrivat(user.privat)
+user.startUp();
 
 
